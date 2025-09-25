374a43aa3c7d70c0386872993ab230e14616e594
leobravoe <leonardo.bravo.estacio@gmail.com>
2025-09-25 10:35:19 -0300

    Finalizando a primeira parte

--- src/components/Contador.jsx
+++ src/components/Contador.jsx
@@ -1,14 +1,18 @@
 import { useState } from 'react'
-
 const Contador = () => {
-    const [valor, setValor] = useState(0);
+    const [valor, setValor] = useState(0); // Criando um estado
+    const diminuir = () => {
+        setValor( valor - 1 );
+    }
+    const incrementar = () => {
+        setValor( valor + 1 );
+    }
     return (
         <div>
-            <button type="button" onClick={() => setValor(v => v - 1)}>-</button>
+            <button onClick={diminuir}>-</button>
             <span>{valor}</span>
-            <button type="button" onClick={() => setValor(v => v + 1)}>+</button>
+            <button onClick={incrementar}>+</button>
         </div>
     )
 }
-
 export default Contador
