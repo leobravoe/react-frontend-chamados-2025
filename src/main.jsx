a5eeeac66a3771dca3fd5bb9743b72a6e3488dec
leobravoe <leonardo.bravo.estacio@gmail.com>
2025-09-25 11:13:02 -0300

    Finalizando a aula 6

--- src/main.jsx
+++ src/main.jsx
@@ -1,5 +1,15 @@
 import { createRoot } from 'react-dom/client'
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import App from './components/App.jsx'
+import Sobre from './pages/Sobre.jsx'
+import Contato from './pages/Contato.jsx';
+
+const router = createBrowserRouter([
+  { path: "/", element: <App /> },
+  { path: "/sobre", element: <Sobre /> },
+  { path: "/contato", element: <Contato /> },
+]);
+
 createRoot(document.getElementById('root')).render(
-  <App />
+  <RouterProvider router={router} />
 )
