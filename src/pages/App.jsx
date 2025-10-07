53b1fe21e478810dfcf7b027b20e337b82323b6a
leobravoe <leonardo.bravo.estacio@gmail.com>
2025-10-07 17:22:30 -0300

    Adicionando o Navbar

--- src/pages/App.jsx
+++ src/pages/App.jsx
@@ -1,10 +1,12 @@
 import ChamadosList from "../components/ChamadosList"
+import Navbar from "../components/Navbar"
 
 // src/App.jsx
 const App = () => {
   return (
     <>
       <h1>
+        <Navbar />
         <ChamadosList />
       </h1>
     </>
