307e70cc66c2c0dd57a4ba271faaf975588bee5a
leobravoe <leonardo.bravo.estacio@gmail.com>
2025-10-23 10:36:52 -0300

    adicionando os estados e vinculando seus vaores aos inputs

--- src/components/ChamadoFormCreate.jsx
+++ src/components/ChamadoFormCreate.jsx
@@ -1,6 +1,10 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 const ChamadoFormCreate = () => {
+    const [Usuarios_id, setUsuariosId] = useState("1");
+    const [texto, setTexto] = useState("");
+    const [estado, setEstado] = useState("a");
+    const [url_imagem, setUrlImagem] = useState("");
 
     const handleSubmit = (e) => {
         e.preventDefault();
@@ -11,24 +15,47 @@ const ChamadoFormCreate = () => {
         <form onSubmit={handleSubmit} className='m-2'>
             <div className='my-2'>
                 <label className='form-label' htmlFor="id-input-Usuarios_id">Usuarios_id</label>
-                <input className='form-control' type="text" id="id-input-Usuarios_id" />
+                <input
+                    className='form-control'
+                    type="text"
+                    id="id-input-Usuarios_id"
+                    value={Usuarios_id}
+                    onChange={(e) => setUsuariosId(e.target.value)}
+                />
             </div>
             <div className='my-2'>
-                <label className='form-label' htmlFor="id-input-Usuarios_id">Usuarios_id</label>
-                <input className='form-control' type="text" id="id-input-Usuarios_id" />
+                <label className='form-label' htmlFor="id-input-texto">texto</label>
+                <input
+                    className='form-control'
+                    type="text"
+                    id="id-input-texto"
+                    value={texto}
+                    onChange={(e) => setTexto(e.target.value)}
+                />
             </div>
             <div className='my-2'>
-                <label className='form-label' htmlFor="id-input-Usuarios_id">Usuarios_id</label>
-                <input className='form-control' type="text" id="id-input-Usuarios_id" />
+                <label className='form-label' htmlFor="id-input-estado">estado</label>
+                <input
+                    className='form-control'
+                    type="text"
+                    id="id-input-estado"
+                    value={estado}
+                    onChange={(e) => setEstado(e.target.value)}
+                />
             </div>
             <div className='my-2'>
-                <label className='form-label' htmlFor="id-input-Usuarios_id">Usuarios_id</label>
-                <input className='form-control' type="text" id="id-input-Usuarios_id" />
+                <label className='form-label' htmlFor="id-input-url_imagem">url_imagem</label>
+                <input
+                    className='form-control'
+                    type="text"
+                    id="id-input-url_imagem"
+                    value={url_imagem}
+                    onChange={(e) => setUrlImagem(e.target.value)}
+                />
             </div>
-            <button>Sem tipo</button>
-            <button type='button'>Tipo button</button>
-            <button type='reset'>Tipo reset</button>
-            <button type='submit' className='btn btn-primary'>Enviar</button>
+            <div className='my-2'>
+                <button type='submit' className='btn btn-primary'>Enviar</button>
+            </div>
         </form>
     )
 }
