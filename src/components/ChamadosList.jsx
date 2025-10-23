59e5f095c24b83f854f199096b80b324f51ed447
leobravoe <leonardo.bravo.estacio@gmail.com>
2025-10-23 07:55:03 -0300

    Final da aula 9

--- src/components/ChamadosList.jsx
+++ src/components/ChamadosList.jsx
@@ -1,51 +1,82 @@
-import { useEffect, useState } from 'react'
+// src/components/ChamadosList.jsx
+import { useState, useEffect } from 'react';
 import Chamado from './Chamado';
-
+// ChamadosList com cache no localStorage
 const ChamadosList = () => {
     // 1. Estados para guardar os dados, o status de carregamento e possíveis erros
-    const [chamados, setChamados] = useState([]);
-    const [loading, setLoading] = useState(true);
+    const [chamados, setChamados] = useState(JSON.parse(localStorage.getItem('chamadosCache')) ?? []);
+    const [loading, setLoading] = useState(JSON.parse(localStorage.getItem('chamadosCache')) ? false : true);
     const [error, setError] = useState(null);
-
-    // Chamado depois do primeiro RENDER
+    // 2. O useEffect para buscar os dados
     useEffect(() => {
+        // Cria um AbortController para gerenciar a requisição
         const abortController = new AbortController();
         // Define função assíncrona para realizar a busca
         const fetchChamados = async () => {
             try {
-                // A URL completa da nossa API
-                const response = await fetch('http://localhost:3000/api/chamados', 
-                    { signal: abortController.signal }
-                ); // ERRO
-                if (!response.ok) {
-                    throw new Error(`Erro HTTP: ${response.status}`);
-                }
-                const data = await response.json();
+                // A URL completa da nossa API, agora com o sinal do controller
+                const response = await fetch('http://localhost:3000/api/chamados', {
+                    signal: abortController.signal
+                });
+                // Se houver erro na resposta
+                if (!response.ok) throw new Error(`Erro HTTP: ${response.status}`);
+                // Se não houver atualização nos dados
+                if (response.status == 304) return;
+                const data = await response.json(); // Converte para JSON
                 setChamados(data); // Guarda os dados no estado
+                setError(null);    // Limpa algum erro
+                localStorage.setItem('chamadosCache', JSON.stringify(data)); // Guarda no localStorage
             } catch (err) {
-                setError(err.message); // Guarda a mensagem de erro no estado
+                setError(err.message); // Seta o estado de erro
             } finally {
-                setLoading(false); // Finaliza o carregamento, com sucesso ou erro
-            }
-            return () => {
-                abortController.abort();
+                setLoading(false); // Finaliza o carregamento, com sucesso, erro ou cancelamento
             }
         };
-        fetchChamados(); // Executa a função
-    }, []);
-
+        // Caso não haja cache armazenado
+        if (!JSON.parse(localStorage.getItem('chamadosCache')))
+            fetchChamados(); // Executa a função a primeira vez
+        // A cada 5 segundos fetchChamados() será chamada
+        const interval5secs = setInterval(() => {
+            fetchChamados(); // Executa a função de tempos em tempos
+        }, 5000);
+        // 3. A função de limpeza do useEffect.
+        //    Será chamada quando o componente for desmontado.
+        return () => {
+            abortController.abort(); // Cancela a requisição fetch em andamento
+            clearInterval(interval5secs);
+        };
+    }, []); // O array de dependências vazio garante que o efeito rode apenas uma vez
+    // 4. Renderização condicional do componente
     if (loading) {
-        return <p>Carregando chamados...</p>
+        return <p>Carregando chamados...</p>;
     }
-    if (error) {
-        return <p>Erro ao buscar dados: {error}</p>
-    }
+    // 5. Renderização do componente
     return (
         <div>
-            {
-                chamados.map(chamado => <Chamado key={chamado.id} chamado={chamado} /> )
-            }
+            <h1>Lista de Chamados</h1>
+            {error && <div class="toast-container position-fixed bottom-0 end-0 p-3">
+                <div class="toast show" role="alert" aria-live="assertive" aria-atomic="true">
+                    <div class="toast-header">
+                        <strong class="me-auto">Erro</strong>
+                        <button
+                            type="button"
+                            class="btn-close"
+                            aria-label="Close"
+                            onClick={() => setError(null)}
+                        >
+                        </button>
+                    </div>
+                    <div class="toast-body">
+                        {error}
+                    </div>
+                </div>
+            </div>}
+            <div>
+                {chamados.map((chamado) => (
+                    <Chamado key={chamado.id} chamado={chamado} />
+                ))}
+            </div>
         </div>
-    )
-}
-export default ChamadosList
+    );
+};
+export default ChamadosList;
