38a68f501f9387ba92bb3ced3e550ad8bb69a04a
leobravoe <leonardo.bravo.estacio@gmail.com>
2025-10-23 09:47:48 -0300

    Programando o FormCreate

--- src/components/ChamadoFormCreate.jsx
+++ src/components/ChamadoFormCreate.jsx
@@ -1,9 +1,36 @@
 import React from 'react'
 
 const ChamadoFormCreate = () => {
-  return (
-    <div>ChamadoFormCreate</div>
-  )
+
+    const handleSubmit = (e) => {
+        e.preventDefault();
+        console.log("Função chamada");
+    }
+
+    return (
+        <form onSubmit={handleSubmit} className='m-2'>
+            <div className='my-2'>
+                <label className='form-label' htmlFor="id-input-Usuarios_id">Usuarios_id</label>
+                <input className='form-control' type="text" id="id-input-Usuarios_id" />
+            </div>
+            <div className='my-2'>
+                <label className='form-label' htmlFor="id-input-Usuarios_id">Usuarios_id</label>
+                <input className='form-control' type="text" id="id-input-Usuarios_id" />
+            </div>
+            <div className='my-2'>
+                <label className='form-label' htmlFor="id-input-Usuarios_id">Usuarios_id</label>
+                <input className='form-control' type="text" id="id-input-Usuarios_id" />
+            </div>
+            <div className='my-2'>
+                <label className='form-label' htmlFor="id-input-Usuarios_id">Usuarios_id</label>
+                <input className='form-control' type="text" id="id-input-Usuarios_id" />
+            </div>
+            <button>Sem tipo</button>
+            <button type='button'>Tipo button</button>
+            <button type='reset'>Tipo reset</button>
+            <button type='submit' className='btn btn-primary'>Enviar</button>
+        </form>
+    )
 }
 
 export default ChamadoFormCreate
